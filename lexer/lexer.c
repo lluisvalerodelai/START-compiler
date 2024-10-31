6c87828a95d6b556fac0ae496144cd7775c0eae5
Luis Valero <lluisvalerodelai@gmail.com>
2024-10-31 19:27:00 +0100

    random changes

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,9 +1,11 @@
 #include "lexer.h"
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 enum token_type;
 
